b9ff46db9f2ef0f05e9195039a7f2eeaec972b46
pehchankon <sankalp.sahu.04@gmail.com>
2023-11-01 22:35:14 +0530

    random fix

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../widgets/loader.widget.dart';
 import 'searchEvents.page.dart';
